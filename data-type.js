6a8dab5800b81b8b5148702a69b91d8d437c0340
hkw2304 <111471255+hkw2304@users.noreply.github.com>
2023-11-18 18:24:07 +0900

    데이터 타입

---
+++ data-type.js
@@ -0,0 +1,29 @@
+
+//숫자형
+
+console.log(1 === 1.0); //true가 출력
+console.log(4 / 2); // 2
+console.log(3 / 2) // 1.5
+
+//문자열
+
+var string;
+string = "str";
+console.log(string);
+string = "str2";
+console.log(string);
+string = "str3";
+console.log(string);
+// stirng = str4;
+
+
+//문자열 삽입
+
+var first = "hello";
+var second = "world!!";
+console.log(first + second);
+//helloworld!!
+
+// 템플릿리터널
+
+console.log(`1 + 2 = ${1 + 2}`);
