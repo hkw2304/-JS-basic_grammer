f5c289ef4e6f1993967861c5609e514d0a8abbcc
hkw2304 <111471255+hkw2304@users.noreply.github.com>
2023-11-25 11:25:59 +0900

    map, filter, reduce

--- callback.js
+++ callback.js
@@ -11,3 +11,23 @@ function logAll(i){
 repeat(5, logAll);
 
 //함수의 매개변수를 통해 다른 함수의 내부로 전달되는 함수이다.
+
+//map
+
+var arr = [1,2,3];
+arr = arr.map(function(item){
+    return item * 2;
+});
+console.log(arr); // 2 4 6
+
+arr = [1,2,3];
+arr = arr.filter(function(item){
+    return item % 2;
+});
+console.log(arr); // 1 3
+
+arr = [1,2,3];
+arr = arr.reduce(function(arr, cur){
+    return arr + cur;
+});
+console.log(arr);
