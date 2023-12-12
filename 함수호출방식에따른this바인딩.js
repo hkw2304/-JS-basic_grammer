a1bb58ca84fcbe4869b4d3067a6a128d96afaa8f
hkw2304 <111471255+hkw2304@users.noreply.github.com>
2023-12-12 19:59:54 +0900

    호출에따른 바인딩

--- 함수호출방식에따른this바인딩.js
+++ 함수호출방식에따른this바인딩.js
@@ -31,3 +31,23 @@ obj.foo2();
 
 // 일반 함수로 호출된 모든 함수(중첩, 콜백) 내부의 this에는 전역 객체가 바인딩된다.
 
+
+
+
+function Circle(radius){
+    this.radius = radius;
+    this.getDiameter = function(){
+        return 2 * this.radius;
+    };
+}
+
+const circle1 = new Circle(5);
+const circle2 = new Circle(10);
+
+console.log(circle1.getDiameter());
+console.log(circle2.getDiameter());
+
+const circle3 = Circle(15);
+// return 값이 없어 undefined
+console.log(circle3); 
+console.log(radius);
